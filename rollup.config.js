ce3489351a4018807b9a4cd620948509535ba1f8
Katherine Norton <katherine@queerkat.games>
2023-09-22 10:18:46 -0500

    Update and reorganize info.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import { exec } from "child_process";
 import { homedir } from "os";
 import { promisify } from "util";
-import { filename } from "./src/info.js";
+import { name, version } from "./src/info.js";
 
 
 const options =
@@ -12,7 +12,7 @@ const options =
 	/**
 	 * Change the file name of the output file here.
 	 */
-	filename: filename,
+	filename: `${name}-v${version}.js`,
 
 	/**
 	 * Determines in what build mode the plugin should be build. The default here takes
--- src/info.js
+++ src/info.js
@@ -1,15 +1,14 @@
-export const name = "Name of your plugin";
+export const name = "name-of-your-plugin";
 export const authors = [ "Your name" ];
-export const version = "1.0.0";
 export const license = "MIT";
 
-export const filename = `my-plugin-v${version}.js`;
+export const version = "1.0.0";
+export const type = "remote";
 
 /**
- * The following fields determine which OpenRCT2 API version to use. It's best to always target
+ * The following field determines which OpenRCT2 API version to use. It's best to always target
  * the latest release version, unless you want to use specific versions from a newer develop
- * version. Version 77 equals the v0.4.5 release.
- * @see https://github.com/OpenRCT2/OpenRCT2/blob/v0.4.5/src/openrct2/scripting/ScriptEngine.h#L50
+ * version. Version 78 equals the v0.4.6 release.
+ * @see https://github.com/OpenRCT2/OpenRCT2/blob/v0.4.6/src/openrct2/scripting/ScriptEngine.h#L50
  */
-export const targetApiVersion = 77;
-export const type = "remote";
+export const targetApiVersion = 78;
